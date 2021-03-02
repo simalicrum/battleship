81aaa02c85b8262d3900e5cbe519fe2ba2707590
Robert Reinert <robert.reinert@gmail.com>
2021-03-01 17:15:15 -0800

    Updates

---
+++ precache-manifest.05795a8b12aef4a0ed80ab44a9b95a16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2124b679866ae652dcc163a98f4fdca",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "5f725fac624fa5cce222",
+    "url": "/battleship/static/css/main.2d0953b5.chunk.css"
+  },
+  {
+    "revision": "2fab8c8a782b0b7844db",
+    "url": "/battleship/static/js/2.351e32a4.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/battleship/static/js/2.351e32a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f725fac624fa5cce222",
+    "url": "/battleship/static/js/main.d3804e9d.chunk.js"
+  },
+  {
+    "revision": "1576b120f8ea920fbd40",
+    "url": "/battleship/static/js/runtime-main.9e463cc9.js"
+  }
+]);
